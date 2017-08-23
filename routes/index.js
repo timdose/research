0e4801fedbfcfd12c4df67fd554cc918ba4627ba
Tim Dose <timdose@gmail.com>
2017-08-23 02:19:33 -0400

    removed old front end method of uploading

--- routes/index.js
+++ routes/index.js
@@ -145,32 +145,6 @@ function handleTextFileUpload(fields, file) {
   });
 }
 
-router.get('/sign-s3', function(req, res, next){
-    console.log('*********BUCKET: ' + process.env.S3_BUCKET)
-    const s3 = new aws.S3();
-      const fileName = req.query['file-name'];
-      const fileType = req.query['file-type'];
-      const s3Params = {
-        Bucket: S3_BUCKET,
-        Key: fileName,
-        Expires: 60,
-        ContentType: fileType,
-        ACL: 'public-read'
-      };
-
-      s3.getSignedUrl('putObject', s3Params, (err, data) => {
-        if(err){
-          console.log(err);
-          return res.end();
-        }
-        const returnData = {
-          signedRequest: data,
-          url: 'https://${S3_BUCKET}.s3.amazonaws.com/${fileName}'
-        };
-        res.write(JSON.stringify(returnData));
-        res.end();
-      });
-});
 
 router.get('/thank-you', function(req, res, next) {
   res.render('confirm');
